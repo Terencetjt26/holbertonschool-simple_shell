0d81814c3957b3d2d92bc7ffb04af1b98c20f320
ItsZmainDev <zmainpro@gmail.com>
2025-04-22 22:52:42 +0200

    Add function to converts a string to an interger

--- utils.c
+++ utils.c
@@ -50,3 +50,30 @@ int _empty(char *buffer)
 	}
 	return (1);
 }
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ * Return: integer value of the string
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, num = 0;
+
+	while ((s[i] < '0' || s[i] > '9') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+			i++;
+		}
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		num = num * 10 + sign * (s[i] - '0');
+		i++;
+	}
+
+	return (num);
+}
