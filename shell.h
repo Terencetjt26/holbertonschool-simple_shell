9ade7636e98c604807ef0f1c746696e1d4797a65
ItsZmainDev <zmainpro@gmail.com>
2025-04-22 15:33:01 +0200

    Add prototypes

--- shell.h
+++ shell.h
@@ -11,5 +11,15 @@
 
 /* PROTOTYPES */
 int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+int _empty(char *buffer);
+char **_split(char *str, char *sep);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char **tokenize(char *buffer);
+char *get_path(char *command);
+char *get_env(char *name);
+void _env(void);
+
+extern char **environ;
 
 #endif /* MAIN_H */
