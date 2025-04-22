46d4eb66b02508c62de12cefc16fe9781d3694af
ItsZmainDev <zmainpro@gmail.com>
2025-04-22 15:33:22 +0200

    Add function to get path

---
+++ path.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+/**
+ * get_path - gets the full path of a command
+ * @command: command to find
+ * Return: full path of the command
+ */
+char *get_path(char *command)
+{
+	char *path = getenv("PATH");
+	char *path_copy = strdup(path);
+	char *token = strtok(path_copy, ":");
+	struct stat buffer;
+
+	while (token != NULL)
+	{
+		char *full_path = malloc(strlen(token) + strlen(command) + 2);
+
+		if (full_path == NULL)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+
+		sprintf(full_path, "%s/%s", token, command);
+
+		if (stat(full_path, &buffer) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+
+		free(full_path);
+		token = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return (NULL);
+}
