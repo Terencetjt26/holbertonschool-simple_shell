5caa1b572290123f3c3f7dc7d3af96c851fb73f3
ItsZmainDev <zmainpro@gmail.com>
2025-04-22 15:32:47 +0200

    Add functions to get a strlen and empty line

--- utils.c
+++ utils.c
@@ -18,3 +18,35 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (0);
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _empty - checks if a string is empty
+ * @buffer: string to check
+ * Return: 1 if empty, 0 if not
+ */
+int _empty(char *buffer)
+{
+	int i = 0;
+
+	while (buffer[i])
+	{
+		if (buffer[i] != ' ')
+			return (0);
+		i++;
+	}
+	return (1);
+}
