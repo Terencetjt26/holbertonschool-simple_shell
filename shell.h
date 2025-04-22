e3283ac609d16b97be5ee4cba3faeb70c04cd12d
ItsZmainDev <zmainpro@gmail.com>
2025-04-22 22:53:51 +0200

    Update prototypes for the builtins

--- shell.h
+++ shell.h
@@ -9,16 +9,50 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+/* STRUCTURES */
+/**
+ * struct sh_t - structure pour les données du shell
+ * @args: arguments de la commande
+ * @status: statut de la dernière commande exécutée
+ * @buffer: ligne lue depuis l'entrée standard
+ */
+typedef struct sh_t
+{
+	char **args;
+	int status;
+	char *buffer;
+} sh_t;
+
+/**
+ * struct builtin_t - structure pour les commandes internes
+ * @name: nom de la commande interne
+ * @f: pointeur vers la fonction qui implémente la commande
+ */
+typedef struct builtin_t
+{
+	char *name;
+	int (*f)(sh_t *data);
+} builtin_t;
+
 /* PROTOTYPES */
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
 int _empty(char *buffer);
 char **_split(char *str, char *sep);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int execute_command(char **args);
 char **tokenize(char *buffer);
 char *get_path(char *command);
 char *get_env(char *name);
 void _env(void);
+int _atoi(char *s);
+
+/* Builtin handlers */
+
+int handle_builtin(sh_t *data);
+int builtin_exit(sh_t *data);
+int builtin_cd(sh_t *data);
+int builtin_env(sh_t *data);
 
 extern char **environ;
 
