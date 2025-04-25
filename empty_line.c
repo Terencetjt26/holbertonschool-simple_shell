74274e76803fcf57f6773f53d632d17c63a00086
ItsZmainDev <zmainpro@gmail.com>
2025-04-25 10:30:21 +0200

    Fix betty

--- empty_line.c
+++ empty_line.c
@@ -5,15 +5,15 @@
  * @buffer: string to check
  * Return: 1 if empty, 0 if not
  */
- int _empty(char *buffer)
- {
-	 int i = 0;
- 
-	 while (buffer[i])
-	 {
-		 if (buffer[i] != ' ')
-			 return (0);
-		 i++;
-	 }
-	 return (1);
- }
+int _empty(char *buffer)
+{
+	int i = 0;
+
+	while (buffer[i])
+	{
+		if (buffer[i] != ' ')
+			return (0);
+		i++;
+	}
+	return (1);
+}
